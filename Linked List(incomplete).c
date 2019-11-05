c8b88cf9027e06a13d49bc99bec89c79acc3ed4c
ivirmani <43945136+ivirmani@users.noreply.github.com>
2019-11-05 22:53:34 +0530

    Update Linked List(incomplete).c

--- Linked List(incomplete).c
+++ Linked List(incomplete).c
@@ -1,124 +1,168 @@
-#include<stdio.h>
-#include<stdlib.h>
-int i=0;
+#include <stdio.h>
+#include <stdlib.h>
 struct node
 {
-	int data;
-	struct node *link;
-};struct node *ptr,*start=NULL,*temp,*cur;
-void insert_beg()
-{
-	temp=(struct node*)malloc(sizeof(struct node));
-	printf("Enter the data\n");
-	scanf("%d",&(temp->data));
-	temp->link=NULL;
-	if(start==NULL)
-		start=temp;
-	else
-	{
-		temp->link=start;
-		start=temp;
-	}
-}
+    int data;
+    struct node *link;
+};
+struct node *start=NULL,*temp,*ptr,*cur;
 void insert_end()
 {
-	temp=(struct node*)malloc(sizeof(struct node));
-	printf("Enter the data\n");
-	scanf("%d",&(temp->data));
-	temp->link=NULL;
-	if(start==NULL)
-		start=temp;
-	else
-	{
-		ptr=start;
-		while(ptr->link!=NULL)
-		{
-			ptr=ptr->link;
-		}
-		ptr->link=temp;
-	}
+    temp=(struct node*)malloc(sizeof(struct node));
+    printf("Enter Node Data\n");
+    scanf("%d",&temp->data);
+    temp->link=NULL;
+    if(start==NULL)
+        start=temp;
+    else
+    {
+        ptr=start;
+        while(ptr->link!=NULL)
+        {
+            ptr=ptr->link;
+        }
+        ptr->link=temp;
+    }
 }
-void delete_beg()
+void insert_pos()
+
 {
-	if(start==NULL)
-		printf("Empty\n");
-	else
-	{
-		if(start->link==NULL)
-		{
-			start=NULL;
-		}
-		ptr=start;
-		start=start->link;
-		free(ptr);
-	}
-}
-void delete_pos()
+    temp=(struct node*)malloc(sizeof(struct node));
+    printf("\nEnter Node Data\n");
+    scanf("%d",&temp->data);
+    temp->link=NULL;
+    int count=1,i=0;
+    ptr=start;
+    while(ptr->link!=NULL)
+    {
+        ptr=ptr->link;
+        count++;
+    }
+    printf("\nEnter the position of node to be inserted\n");
+    int pos1;
+    scanf("%d",&pos1);
+    if(pos1==1)
+        insert_beg();
+    else if(pos1==count)
+        insert_end();
+    else if(pos1>1&&pos1<count)
+    {
+        ptr=start;
+        for(i=1;i<pos1;i++)
+        {
+            cur=ptr;
+            ptr=ptr->link;
+        }
+
+        cur->link=temp;
+        temp->link=ptr;
+
+    }
+    else
+        printf("\nInvalid position entered\n");
+
+    }
+
+void insert_beg()
 {
-	int count=0,i,pos;
-	ptr=start;
-	while(ptr!=NULL)
-	{
-		count++;
-		ptr=ptr->link;
-	}
-	printf("Enter the position of ther node to be deleted\n");
-	scanf("%d",&pos);
-	if(pos==1)
-		delete_beg();
-	else if(pos==count)
-		delete_end();
-	else if(pos>1&&pos<count)
-	{
-		ptr=start;
-		for(i=2;i<pos;i++)
-			ptr=ptr->link;
-			temp=ptr->link;
-			ptr->link=temp->link;
-			printf("\nDeleted %d\n",temp->data);	
-			free(temp);
-			count--;
-		
-	}
+    temp=(struct node*)malloc(sizeof(struct node));
+    printf("Enter The Data\n");
+    scanf("%d",&temp->data);
+    temp->link=NULL;
+    if(start==NULL)
+        start=temp;
+    else
+    {
+        temp->link=start;
+        start=temp;
+    }
 }
 void delete_end()
 {
-	if(start==NULL)
-		printf("Empty\n");
-	else
-	{
-		ptr=start;
-		while(ptr->link!=NULL)
-		{
-			cur=ptr;
-			ptr=ptr->link;
-		}
-		cur->link=NULL;
-		free(ptr);	
-	}
+    if(start==NULL)
+        printf("Linked list is empty\n");
+    else
+    {
+        ptr=start;
+        while(ptr->link!=NULL)
+        {
+            cur=ptr;
+            ptr=ptr->link;
+        }
+        printf("\nDeleted element is %d\n",ptr->data);
+        cur->link=NULL;
+        free(ptr);
+    }
+}
+void delete_beg()
+{
+    if(start==NULL)
+        printf("Linked list is empty\n");
+    else
+    {
+        temp=start;
+        start=start->link;
+        printf("\nDeleted element is %d\n",temp->data);
+        free(temp);
+    }
+}
+void delete_pos()
+{
+    int count=1,i=0;
+    ptr=start;
+    while(ptr->link!=NULL)
+    {
+        ptr=ptr->link;
+        count++;
+    }
+    printf("\nEnter the position of node to be deleted\n");
+    int pos;
+    scanf("%d",&pos);
+    if(pos==1)
+        delete_beg();
+    else if(pos==count)
+        delete_end();
+    else if(pos>1&&pos<count)
+    {
+        ptr=start;
+        for(i=1;i<pos;i++)
+        {
+            cur=ptr;
+            ptr=ptr->link;
+        }
+
+        cur->link=ptr->link;
+        printf("\nDeleted element is %d\n",ptr->data);
+
+        free(ptr);
+
+    }
+    else
+        printf("\nInvalid position entered\n");
 }
 void display()
+
 {
-	
-	if(start==NULL)
+    if(start==NULL)
 		printf("List Is Empty\n\n");
 	else
 	{
 		ptr=start;
 		while(ptr!=NULL)
 		{
-			
-			printf("%d\n",ptr->data);
+
+			printf("%d-->",ptr->data);
 			ptr=ptr->link;
 		}
 	}
+
 }
 void main()
 {
-	int ch;
+    int ch;
         while(1)
 	{
-		printf("1.Insert Beginning\n2.Insert End\n3.Display\n4.Delete End\n5.Delete Beginning\n6.Delete any position\nEnter The Choice\n");
+		printf("1.Insert Beginning\n2.Insert End\n3.Insert position\n4.Delete End\n5.Delete Beginning\n6.Delete any position\n7.Display\n8.Exit\nEnter The Choice\n");
 		scanf("%d",&ch);
 		switch(ch)
 		{
@@ -126,7 +170,7 @@ void main()
 		      break;
 		case 2:insert_end();
 		      break;
-		case 3:display();
+		case 3:insert_pos();
 		      break;
 		case 4:delete_end();
 		      break;
@@ -134,6 +178,9 @@ void main()
 		      break;
 		case 6:delete_pos();
 		      break;
+        case 7:display();
+               break;
+        case 8:exit(0);
 		}
 	}
 }
