865d89c5e97966199cdbef181cf8fd345ca95dbc
Ishaan Virmani <ivirmani@gmail.com>
2019-12-18 19:39:20 +0530

    Update Infix To Postfix.c

--- Infix To Postfix.c
+++ Infix To Postfix.c
@@ -7,84 +7,88 @@ char s[MAX],infix[MAX],postfix[MAX],ch,temp;
 int top=-1,i=0,j=0;
 void push(char ch)
 {
-	top+=1;
-	s[top]=ch;
+    top+=1;
+    s[top]=ch;
 }
 char pop()
 {
-	char temp;
-	temp=s[top--];
-	return temp;
+    char temp;
+    temp=s[top--];
+    return temp;
 }
 int isoperator(char ch)
 {
-	if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='^')
-		return(1);
-	else
-		return(0);
+    if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='^')
+        return(1);
+    else
+        return(0);
 }
 int precedence(char ch)
 {
-	switch(ch)
-	{
-		case '^':return(4);
-		         break;
-		case '*':
-		case '/':return(3);
-			 break;
-		case '+':
-		case '-':return(2);
-			 break;
-		default:return(1);
-	}
+    switch(ch)
+    {
+    case '^':
+        return(4);
+        break;
+    case '*':
+    case '/':
+        return(3);
+        break;
+    case '+':
+    case '-':
+        return(2);
+        break;
+    default:
+        return(1);
+    }
 }
 void main()
 {
-	printf("\nEnter The Expression\n\n");
-	gets(infix);
-	while(infix[i]!='\0')
-	{
-		ch=infix[i];
-		if(isalpha(ch))
-		{
-			postfix[j]=ch;
-			j++;
-		}
-		else if(ch=='(')
-		{
-			push(ch);
-		}
-		else if(isoperator(ch))
-		{
-			if(ch!='^')
-			{
-				while(top!=-1&&precedence(s[top])>=precedence(ch))
-				{
-					postfix[j]=pop();
-					j++;
-				}
-			}
-			push(ch);
+    printf("\nEnter The Expression\n\n");
+    gets(infix);
+    while(infix[i]!='\0')
+    {
+        ch=infix[i];
+        if(isalpha(ch))
+        {
+            postfix[j]=ch;
+            j++;
         }
-	else if(ch==')')
-	{
-		while(s[top]!='(')
-		{
-			postfix[j]=pop();
-			j++;
-		}
-		temp=pop();
-	}
-	else
-		printf("Invalid Character\n");
-	i++;
-    	}
-	while(top>-1)
-	{
-		postfix[j]=pop();
-		j++;
-	}
-	postfix[j]='\0';
-	printf("The Postfix Expression Is ");
-	puts(postfix);
+        else if(ch=='(')
+        {
+            push(ch);
+        }
+        else if(isoperator(ch))
+        {
+            if(ch!='^')
+            {
+                while(top!=-1&&precedence(s[top])>=precedence(ch))
+                {
+                    postfix[j]=pop();
+                    j++;
+                }
+            }
+            push(ch);
+        }
+        else if(ch==')')
+        {
+            while(s[top]!='(')
+            {
+                postfix[j]=pop();
+                j++;
+            }
+            temp=pop();
+        }
+        else
+            printf("Invalid Character\n");
+        i++;
+    }
+    while(top>-1)
+    {
+        postfix[j]=pop();
+        j++;
+    }
+    postfix[j]='\0';
+    printf("The Postfix Expression Is ");
+    puts(postfix);
 }
